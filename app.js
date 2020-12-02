ac0810f7735c050d70d52f96e7baa37a18fc4ef8
Andreas_Just <andreas.wq@gmail.com>
2020-12-02 16:16:58 +0200

    process.env.NODE_ENV === 'production' 11-17

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const config = require('config');
+const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(require('./routes/routes'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile((path.resolve(__dirname, 'client', 'build', 'index.html')));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
