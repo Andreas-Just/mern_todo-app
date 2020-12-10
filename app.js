02fb4295959309a6f883a4871fe0813e8ce12be1
Andreas_Just <andreas.wq@gmail.com>
2020-12-10 20:38:49 +0200

    cross-env was deleted, from app.js was deleted condition process.env.NODE_ENV === 'production'

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const config = require('config');
-const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(require('./routes/routes'));
-
-if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile((path.resolve(__dirname, 'client', 'build', 'index.html')));
-  });
-}
+app.use(express.static('client/build'));
 
 const PORT = process.env.PORT || 5000;
 
