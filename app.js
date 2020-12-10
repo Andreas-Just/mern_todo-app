8caa3534d6aff739e921f9098651cd1946f247fc
Andreas_Just <andreas.wq@gmail.com>
2020-12-10 22:41:38 +0200

    condition process.env.NODE_ENV === 'production' was added

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(require('./routes/routes'));
-app.use(express.static('client/build'));
+// app.use(express.static('client/build'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile((path.resolve(__dirname, 'client', 'build', 'index.html')));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
