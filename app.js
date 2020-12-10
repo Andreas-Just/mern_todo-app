e82f2a7da18bbcd52f13b26e3432550cbe647a22
Andreas_Just <andreas.wq@gmail.com>
2020-12-10 21:58:52 +0200

    process.env.MONGO_URI was added

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const start = async() => {
   try {
-    await mongoose.connect(mongoUri, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
